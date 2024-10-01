f12365f93d2c7dbcfe3066277e34513a642fbac7
George Herbert <github@georgeherby.dev>
2024-10-01 19:01:19 +0100

    chore: add info for setting up latency trigger and encoding collection

--- packages/ui/src/services/projects-service.ts
+++ packages/ui/src/services/projects-service.ts
@@ -1,5 +1,5 @@
 import config from '@/lib/utils/config';
-import { ProjectResponse } from '@/models/responses/projects-response';
+import { ProjectResponse } from '@langscout/models';
 
 export async function getProjects(): Promise<ProjectResponse[]> {
   try {
